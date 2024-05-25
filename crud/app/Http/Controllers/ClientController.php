2175757db14e8bec22e58471c6b22d70eca3f7f6
Caio Rafael <ccaiocr@gmail.com>
2024-05-25 16:45:53 -0300

    ClienteController

--- crud/app/Http/Controllers/ClientController.php
+++ crud/app/Http/Controllers/ClientController.php
@@ -4,93 +4,115 @@ namespace App\Http\Controllers;
 
 use App\Models\Client;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use Carbon\Carbon;
 
 class ClientController extends Controller
 {
+    public function index()
+    {
+        $clients = Client::all();
+        return view('clients.index', compact('clients'));
+    }
+
     public function create()
     {
-        return view('clients.create');
+        $fields = $this->getFields();
+        return view('clients.create', compact('fields'));
     }
 
-    public function index()
-{
-    $clients = Client::all();
-
-    return view('clients.index', compact('clients'));
-}
-
-public function store(Request $request)
-{
-    $request->validate([
-        'nome' => 'required',
-        'email' => 'required|email',
-        'cpf_cnpj' => 'required',
-        'data_nascimento' => 'required',
-        'nome_social' => 'required',
-        'foto' => 'nullable|file',
-    ]);
-
-    $data = [];
-
-    if ($request->hasFile('foto')) {
-        $foto = $request->file('foto');
-        $filename = time() . '.' . $foto->getClientOriginalExtension();
-        $foto->move(public_path('images/clients'), $filename);
-        $data['foto'] = 'images/clients/' . $filename;
-    }
-
-    $data['nome'] = $request->input('nome');
-    $data['email'] = $request->input('email');
-    $data['cpf_cnpj'] = $request->input('cpf_cnpj');
-    $data['data_nascimento'] = $request->input('data_nascimento');
-    $data['nome_social'] = $request->input('nome_social');
-
-    Client::create($data);
-
-    return redirect()->route('clients.index');
-}
-
-public function destroy(Client $client)
+    public function store(Request $request)
     {
-        $client->delete();
+        $validatedData = $this->validateClient($request);
 
-        return redirect()->route('clients.index');
+        DB::transaction(function () use ($validatedData, $request) {
+            if ($request->hasFile('foto')) {
+                $validatedData['foto'] = $this->uploadFile($request->file('foto'));
+            }
+
+            $validatedData['data_nascimento'] = Carbon::createFromFormat('d/m/Y', $request->input('data_nascimento'))->format('Y-m-d');
+
+            Client::create($validatedData);
+        });
+
+        return redirect()->route('clients.index')->with('success', 'Cliente cadastrado com sucesso!');
     }
 
-    public function edit(Client $client)
+    public function edit($id)
     {
-        return view('clients.edit', compact('client'));
+        $client = Client::findOrFail($id);
+        $client->data_nascimento = Carbon::parse($client->data_nascimento)->format('d/m/Y');
+        $fields = $this->getFields();
+        return view('clients.edit', compact('client', 'fields'));
     }
 
     public function update(Request $request, Client $client)
     {
-        $request->validate([
-            'nome' => 'required',
-            'email' => 'required|email',
-            'cpf_cnpj' => 'required',
-            'data_nascimento' => 'required',
-            'nome_social' => 'required',
-            'foto' => 'nullable|file',
-        ]);
+        $validatedData = $this->validateClient($request);
 
-        $data = [];
+        DB::transaction(function () use ($validatedData, $request, $client) {
+            if ($request->hasFile('foto')) {
+                $validatedData['foto'] = $this->uploadFile($request->file('foto'), $client->foto);
+            }
 
-        if ($request->hasFile('foto')) {
-            $foto = $request->file('foto');
-            $filename = time() . '.' . $foto->getClientOriginalExtension();
-            $foto->move(public_path('images/clients'), $filename);
-            $data['foto'] = 'images/clients/' . $filename;
-        }
+            $validatedData['data_nascimento'] = Carbon::createFromFormat('d/m/Y', $request->input('data_nascimento'))->format('Y-m-d');
 
-        $data['nome'] = $request->input('nome');
-        $data['email'] = $request->input('email');
-        $data['cpf_cnpj'] = $request->input('cpf_cnpj');
-        $data['data_nascimento'] = $request->input('data_nascimento');
-        $data['nome_social'] = $request->input('nome_social');
-
-        $client->update($data);
+            $client->update($validatedData);
+        });
 
         return redirect()->route('clients.index')->with('success', 'Cliente atualizado com sucesso!');
     }
-    
-};
+
+    public function delete(Client $client)
+    {
+        if ($client->foto) {
+            Storage::delete($client->foto);
+        }
+
+        $client->delete();
+        return redirect()->route('clients.index')->with('success', 'Cliente deletado com sucesso!');
+    }
+
+    private function validateClient(Request $request)
+    {
+        $rules = [
+            'nome' => 'required',
+            'email' => 'required|email',
+            'cpf_cnpj' => 'required',
+            'data_nascimento' => 'required|date_format:d/m/Y',
+            'nome_social' => 'required',
+            'foto' => 'nullable|file',
+        ];
+
+        if ($request->input('document_type') == 'cpf') {
+            $rules['cpf_cnpj'] = 'required|cpf';
+        } else {
+            $rules['cpf_cnpj'] = 'required|cnpj';
+        }
+
+        return $request->validate($rules);
+    }
+
+    private function uploadFile($file, $oldFile = null)
+    {
+        if ($oldFile) {
+            Storage::delete($oldFile);
+        }
+
+        $filename = time() . '.' . $file->getClientOriginalExtension();
+        return $file->storeAs('images/clients', $filename, 'public');
+    }
+
+    private function getFields()
+    {
+        return [
+            ['name' => 'nome', 'label' => 'Nome', 'type' => 'text', 'required' => true],
+            ['name' => 'data_nascimento', 'label' => 'Data de Nascimento', 'type' => 'date', 'required' => true],
+            ['name' => 'cpf_cnpj', 'label' => 'CPF/CNPJ', 'type' => 'text', 'required' => true],
+            ['name' => 'email', 'label' => 'Email', 'type' => 'email', 'required' => true],
+            ['name' => 'foto', 'label' => 'Foto', 'type' => 'file', 'required' => false],
+            ['name' => 'nome_social', 'label' => 'Nome Social', 'type' => 'text', 'required' => false],
+        ];
+    }
+}
