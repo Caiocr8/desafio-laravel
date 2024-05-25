e4e94dd03803f99c446a86293d841da09beaf9e7
Caio Rafael <ccaiocr@gmail.com>
2024-05-25 17:20:41 -0300

    rotas

--- crud/routes/web.php
+++ crud/routes/web.php
@@ -8,7 +8,10 @@ Route::get('/', [ClientController::class, 'index'])->name('home');
 Route::namespace('App\Http\Controllers')->group(function () {
     Route::resource('clients', ClientController::class);
     
-
+    // Additional routes for CRUD operations
     Route::get('/clients/{client}/edit', [ClientController::class, 'edit'])->name('clients.edit');
-    Route::delete('/clients/{client}', [ClientController::class, 'delete'])->name('clients.delete');
+    Route::put('/clients/{client}', [ClientController::class, 'update'])->name('clients.update');
+    Route::delete('/clients/{client}', [ClientController::class, 'destroy'])->name('clients.delete');
+    Route::get('/clients/create', [ClientController::class, 'create'])->name('clients.create');
+    Route::post('/clients', [ClientController::class, 'store'])->name('clients.store');
 });
